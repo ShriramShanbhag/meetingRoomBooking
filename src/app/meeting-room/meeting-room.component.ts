7dce4fa9afd9a570ac8521f07318d13a09913aea
shriramcai <Shriram.Shanbhag@cai-solutions.com>
2024-06-08 12:41:42 +0530

    [MeetingRoom] Resolved import and binding errors

--- src/app/meeting-room/meeting-room.component.ts
+++ src/app/meeting-room/meeting-room.component.ts
@@ -1,12 +1,29 @@
 import { Component } from "@angular/core";
 import { UpcomingMeetingsComponent } from "./upcoming-meetings/upcoming-meetings.component";
 import { MeetingRoomDetailsComponent } from "./meeting-room-details/meeting-room-details.component";
+import { CreateMeetingDialogComponent } from "./create-meeting-dialog/create-meeting-dialog.component";
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: "app-meeting-room",
   standalone: true,
-  imports: [UpcomingMeetingsComponent, MeetingRoomDetailsComponent],
+  imports: [
+    UpcomingMeetingsComponent,
+    MeetingRoomDetailsComponent,
+    CreateMeetingDialogComponent,
+    NgIf,
+  ],
   templateUrl: "./meeting-room.component.html",
   styleUrl: "./meeting-room.component.css",
 })
-export class MeetingRoomComponent {}
+export class MeetingRoomComponent {
+  showMeetingDialog = false;
+
+  openMeetingDialog = () => {
+    this.showMeetingDialog = true;
+  };
+
+  closeMeetingDialog = () => {
+    this.showMeetingDialog = false;
+  };
+}
