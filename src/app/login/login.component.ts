3f94a0b378f3af4e7c03c28a32ef40e2de5a1363
shriramcai <Shriram.Shanbhag@cai-solutions.com>
2024-06-08 13:30:01 +0530

    [Login] Added login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [FormsModule],
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
 })
 export class LoginComponent {
+  userName: String = "";
+  password: String = "";
 
+  onSubmit() {
+    console.log("Username: ", this.userName);
+    console.log("Password", this.password);
+  }
 }
