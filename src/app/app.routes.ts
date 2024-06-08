1b7e3da020287fb0015386bff52a18152dbc3532
shriramcai <Shriram.Shanbhag@cai-solutions.com>
2024-06-08 23:21:39 +0530

    Added authguard to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+// import { UpcomingMeetingsComponent } from "./meeting-room/upcoming-meetings/upcoming-meetings.component";
+// import { MeetingRoomDetailsComponent } from "./meeting-room/meeting-room-details/meeting-room-details.component";
+import { MeetingRoomComponent } from "./meeting-room/meeting-room.component";
+import { authGuard } from "./auth/auth.guard";
 
-export const routes: Routes = [{ path: "login", component: LoginComponent }];
+export const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  {
+    path: "meetings",
+    component: MeetingRoomComponent,
+    canActivate: [authGuard],
+  },
+];
