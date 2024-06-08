ee077890409a9fcdb3a5140ba8ba7d840c714465
shriramcai <Shriram.Shanbhag@cai-solutions.com>
2024-06-08 10:21:57 +0530

    Added `upcoming meetings` component

---
+++ src/app/meeting-room/upcoming-meetings/upcoming-meetings.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { Meeting } from "../../models/meeting.models";
+import { DatePipe, NgFor } from "@angular/common";
+
+@Component({
+  selector: "app-upcoming-meetings",
+  standalone: true,
+  imports: [NgFor, DatePipe],
+  templateUrl: "./upcoming-meetings.component.html",
+  styleUrl: "./upcoming-meetings.component.css",
+})
+export class UpcomingMeetingsComponent {
+  // constructor(private date: DatePipe) {}
+  upcomingMeetings: Array<Meeting> = [
+    {
+      userName: "demo",
+      agenda: "demo",
+      datetime: new Date(),
+      roomid: "1",
+    },
+  ];
+}
